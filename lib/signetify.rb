d9129d815e740d913774d4da7aa95cb604a1f11a
Peter Karth <falonofthetower@gmail.com>
2017-03-21 23:50:30 -0400

    Basic version working end to end

--- lib/signetify.rb
+++ lib/signetify.rb
@@ -1,5 +1,38 @@
 require "signetify/version"
 
-module Signetify
-  # Your code goes here...
+class Signetify
+  attr_reader :response, :error_message
+  attr_accessor :client
+
+  def initialize(options={})
+    @response = options[:response]
+    @error_message = options[:error_message]
+  end
+
+  def self.authorize(token)
+    client.refresh_token = token
+    client.grant_type = "refresh_token"
+    client.fetch_access_token!
+
+    new(response: client)
+  rescue Signet::AuthorizationError => e
+    new(error_message: e.message)
+  end
+
+  def self.client
+    @client ||= fresh_client
+  end
+
+  def self.client_options
+    template = ERB.new File.new("#{Rails.root}/config/signet.yml").read
+    YAML.load template.result(binding)
+  end
+
+  def self.fresh_client
+    Signet::OAuth2::Client.new(client_options)
+  end
+
+  def successful?
+    response.present?
+  end
 end
--- lib/signetify/version.rb
+++ lib/signetify/version.rb
@@ -1,3 +1,3 @@
-module Signetify
+class Signetify
   VERSION = "0.1.0"
 end
